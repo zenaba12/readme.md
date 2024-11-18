382f8484f42f2597dd7131db155b9d700d6bbec3
zenaba12 <alizenap433@gmail.com>
2024-11-18 02:04:29 +0100

    first commit

---
+++ allRoutes.js
@@ -0,0 +1,7 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
